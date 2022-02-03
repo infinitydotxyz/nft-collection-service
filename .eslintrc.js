d2b292fda482d676027126e3cdb256a091faaf41
Joe Frazier <54604023+jfrazier-eth@users.noreply.github.com>
2022-02-03 12:42:11 -0600

    init

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+    env: {
+      commonjs: true,
+      es6: true,
+      node: true
+    },
+    parser: '@typescript-eslint/parser',
+    plugins: ['promise', '@typescript-eslint'],
+    overrides: [
+        {
+          files: ['*.ts', '*.tsx'], // Your TypeScript files extension
+    
+          // As mentioned in the comments, you should extend TypeScript plugins here,
+          // instead of extending them outside the `overrides`.
+          // If you don't want to extend any rules, you don't need an `extends` attribute.
+          extends: [
+            'plugin:@typescript-eslint/recommended',
+            'plugin:@typescript-eslint/recommended-requiring-type-checking',
+          ],
+    
+          parserOptions: {
+            project: ['./tsconfig.json'], // Specify it only for TypeScript files
+          },
+        },
+      ],
+    extends: [
+      'standard',
+      'standard-with-typescript',
+      'prettier'
+    ],
+    globals: {
+      Atomics: 'readonly',
+      SharedArrayBuffer: 'readonly'
+    },
+    parserOptions: {
+      ecmaVersion: 2020,
+    },
+    rules: {
+      // try commenting out some one of these and fixing any errors worth fixing
+      '@typescript-eslint/strict-boolean-expressions': 0,
+      '@typescript-eslint/explicit-function-return-type': 0,
+      '@typescript-eslint/naming-convention': 0,
+      '@typescript-eslint/no-misused-promises': 0,
+      '@typescript-eslint/restrict-template-expressions': 0
+    },
+    ignorePatterns: ['/dist/**', '**/node_modules/**']
+  };
+  
