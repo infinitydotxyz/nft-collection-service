8b578bd8f04141942658cf7aa5529ccbdf75a4ee
Adi Kancherla <findadit@gmail.com>
2022-02-11 10:41:59 -0500

    minor

--- src/script.ts
+++ src/script.ts
@@ -2,16 +2,14 @@ import ContractFactory from './models/contracts/ContractFactory';
 import CollectionService from './models/CollectionService';
 
 export async function main(): Promise<void> {
+  const address = '0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d'.toLowerCase();
+  const chainId = '1';
 
-    const address = '0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d'.toLowerCase();
-    const chainId = '1';
+  const collectionService = new CollectionService();
+  await collectionService.createCollection(address, chainId);
 
-    const collectionService = new CollectionService();
-    await collectionService.createCollection(address, chainId);
-
-    // const factory = new ContractFactory();
-    // const contract = await factory.create(address, chainId);
-    // const owner = await contract.getOwner();
-    // console.log(owner);
-
+  // const factory = new ContractFactory();
+  // const contract = await factory.create(address, chainId);
+  // const owner = await contract.getOwner();
+  // console.log(owner);
 }
