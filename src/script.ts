b51153e1dcc5619adb7366b72ab4a07d79256b77
Adi Kancherla <findadit@gmail.com>
2022-02-11 08:07:47 -0500

    minor

--- src/script.ts
+++ src/script.ts
@@ -3,15 +3,15 @@ import CollectionService from './models/CollectionService';
 
 export async function main(): Promise<void> {
 
-    const address = '0xdac17f958d2ee523a2206206994597c13d831ec7';
+    const address = '0xdac17f958d2ee523a2206206994597c13d831ec7'.toLowerCase();
     const chainId = '1';
 
-    // const collectionService = new CollectionService();
-    // await collectionService.createCollection(address, chainId);
+    const collectionService = new CollectionService();
+    await collectionService.createCollection(address, chainId);
 
-    const factory = new ContractFactory();
-    const contract = await factory.create(address, chainId);
-    const owner = await contract.getOwner();
-    console.log(owner);
+    // const factory = new ContractFactory();
+    // const contract = await factory.create(address, chainId);
+    // const owner = await contract.getOwner();
+    // console.log(owner);
 
 }
