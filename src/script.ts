696f4c3a84244170cc17e3a40a39b92a0d0fa9de
Joe Frazier <54604023+jfrazier-eth@users.noreply.github.com>
2022-02-11 16:15:16 -0600

    .

--- src/script.ts
+++ src/script.ts
@@ -1,26 +1,9 @@
-
 import CollectionService from './models/CollectionService';
 
 export async function main(): Promise<void> {
   const address = '0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d'.toLowerCase();
   const chainId = '1';
 
-    const collectionService = new CollectionService();
-    await collectionService.createCollection(address, chainId);
-    // try{
-    //     const tokens = await tokenDao.getTokensWithErrors(chainId, address);
-    //     console.log(tokens);
-    // }catch(err) {
-    //     console.error(err);
-    // }
-    
-    // const factory = new ContractFactory();
-    // const contract = await factory.create(address, chainId);
-    // const owner = await contract.getOwner();
-    // console.log(owner);
-
-  // const factory = new ContractFactory();
-  // const contract = await factory.create(address, chainId);
-  // const owner = await contract.getOwner();
-  // console.log(owner);
+  const collectionService = new CollectionService();
+  await collectionService.createCollection(address, chainId);
 }
