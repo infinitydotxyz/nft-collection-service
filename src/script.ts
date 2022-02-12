9d2214f55b4074e19804696bcdfa458064cc4c04
Adi Kancherla <findadit@gmail.com>
2022-02-12 02:51:00 -0500

    minor

--- src/script.ts
+++ src/script.ts
@@ -1,8 +1,8 @@
-import { collectionService } from "./container";
+import { collectionService } from './container';
 
 export async function main(): Promise<void> {
-  const address = '0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d'.toLowerCase();
+  const address = '0x1a92f7381b9f03921564a437210bb9396471050c'.toLowerCase();
   const chainId = '1';
 
-  await collectionService.createCollection(address, chainId);
+  await collectionService.createCollection(address, chainId, true);
 }
