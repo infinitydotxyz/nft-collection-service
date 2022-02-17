52ad771c8cebf260e8d2304abc0e0220793c7bdf
Adi Kancherla <findadit@gmail.com>
2022-02-17 14:55:23 -0500

    changes csv exporter script

--- src/script.ts
+++ src/script.ts
@@ -1,12 +1,17 @@
 import { tokensDataToFile } from './exporters/csv';
-import { collectionService } from './container';
+import { collectionService, firebase } from './container';
 
 export async function main(): Promise<void> {
-  const address = '0x1a92f7381b9f03921564a437210bb9396471050c'.toLowerCase();
-  const chainId = '1';
-
-  await collectionService.createCollection(address, chainId, true);
+  // const address = '0x1a92f7381b9f03921564a437210bb9396471050c'.toLowerCase();
   // const chainId = '1';
-  // const address = '0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d'.toLowerCase();
-  // await tokensDataToFile(chainId, address);
+
+  // await collectionService.createCollection(address, chainId, true);
+  const chainId = '1';
+  const snap = await firebase.db.collection('collections').where('state.create.step', '==', 'complete').get();
+  for (const doc of snap.docs) {
+    const data = doc.data();
+    const address = data.address as string;
+    console.log('fetching data for', address);
+    await tokensDataToFile(chainId, address.toLowerCase());
+  } 
 }
