93c0128ba8b875eedf6670b32ed8759c52b23867
Adi Kancherla <findadit@gmail.com>
2022-02-18 16:58:04 -0500

    minor

--- src/script.ts
+++ src/script.ts
@@ -1,28 +1,26 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { firebase } from './container';
 import BatchHandler from './models/BatchHandler';
 import { addNumOwnersUpdatedAtField } from './background';
 import { createInfuraApiKeys } from './scripts/createInfuraKeys';
 
+// eslint-disable-next-line @typescript-eslint/require-await
 export async function main(): Promise<void> {
   // const address = '0x9e8b85dbb082255bd81c5b25323b694bc799a616'.toLowerCase();
   // const chainId = '1';
   const requests = 0;
-  try{
-
-  /**
-   * must be run to add numOwnersUpdatedAt field to existing collections 
-   * that don't yet have this field
-   */
-  // await addNumOwnersUpdatedAtField();
+  try {
+    /**
+     * must be run to add numOwnersUpdatedAt field to existing collections
+     * that don't yet have this field
+     */
+    // await addNumOwnersUpdatedAtField();
 
     // const numKeys = 45;
     // const namePrefix = 'INFINITY_NFT_COLLECTION_SERVICE';
     // await createInfuraApiKeys(numKeys, namePrefix);
-
-  }catch(err) {
-    console.log(`Failed at ${requests}`)
+  } catch (err) {
+    console.log(`Failed at ${requests}`);
     console.error(err);
   }
-
 }
-
