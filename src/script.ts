812bcb338945a5a5d468ff3f116a329716e2c8fa
Joe Frazier <54604023+jfrazier-eth@users.noreply.github.com>
2022-02-18 13:19:47 -0600

    wrote script to create and format infura ipfs keys

--- src/script.ts
+++ src/script.ts
@@ -1,4 +1,5 @@
 import { addNumOwnersUpdatedAtField } from './background';
+import { createInfuraApiKeys } from './scripts/createInfuraKeys';
 
 export async function main(): Promise<void> {
   // const address = '0x9e8b85dbb082255bd81c5b25323b694bc799a616'.toLowerCase();
@@ -10,8 +11,11 @@ export async function main(): Promise<void> {
    * must be run to add numOwnersUpdatedAt field to existing collections 
    * that don't yet have this field
    */
-  await addNumOwnersUpdatedAtField();
+  // await addNumOwnersUpdatedAtField();
     
+    const numeKeys = 45;
+    const namePrefix = 'INFINITY_NFT_COLLECTION_SERVICE';
+    await createInfuraApiKeys(numeKeys, namePrefix);
 
   }catch(err) {
     console.log(`Failed at ${requests}`)
