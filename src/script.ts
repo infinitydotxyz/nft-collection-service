4e2455d5e60db0107b5ec8b8fd3053536b771d7d
Joe Frazier <54604023+jfrazier-eth@users.noreply.github.com>
2022-02-18 13:22:30 -0600

    changed script back to adding num owners field

--- src/script.ts
+++ src/script.ts
@@ -11,11 +11,11 @@ export async function main(): Promise<void> {
    * must be run to add numOwnersUpdatedAt field to existing collections 
    * that don't yet have this field
    */
-  // await addNumOwnersUpdatedAtField();
+  await addNumOwnersUpdatedAtField();
     
-    const numeKeys = 45;
-    const namePrefix = 'INFINITY_NFT_COLLECTION_SERVICE';
-    await createInfuraApiKeys(numeKeys, namePrefix);
+    // const numKeys = 45;
+    // const namePrefix = 'INFINITY_NFT_COLLECTION_SERVICE';
+    // await createInfuraApiKeys(numKeys, namePrefix);
 
   }catch(err) {
     console.log(`Failed at ${requests}`)
