db12f3c8441c0b992f35f8bd7f096e91b44ec6e0
Joe Frazier <54604023+jfrazier-eth@users.noreply.github.com>
2022-03-02 15:44:55 -0600

    fixed reading hasBlueCheck from file and cli option

--- src/cli.ts
+++ src/cli.ts
@@ -184,10 +184,10 @@ async function fileMode(): Promise<void> {
   const contents = await readFile(filePath, 'utf-8');
   const data = JSON.parse(contents);
 
-  const hasBlueCheck = args.hasBlueCheck === 'true';
+  const hasBlueCheck = args.hasBlueCheck === 'true' ? true : undefined;
   const reset = args.reset === 'true';
 
-  logger.log(`Creating ${data.length} collections. hasBlueCheck: ${hasBlueCheck}`);
+  logger.log(`Creating ${data.length} collections`);
 
   const promises: Array<Promise<void>> = [];
   for (const item of data) {
